b3795826fac2390e71d14630e48dc45945fcd60e
Jefferson Villa Cortés <jeffersonvillacortes@gmail.com>
2024-10-16 13:52:29 -0500

    Commit inicial

---
+++ src/main/java/com/calendario/laboral/ActividadCalendarioLaboralApplication.java
@@ -0,0 +1,13 @@
+package com.calendario.laboral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadCalendarioLaboralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadCalendarioLaboralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calendario/laboral/ActividadCalendarioLaboralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendario.laboral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadCalendarioLaboralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
